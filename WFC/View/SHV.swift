5aedb0f300f8699e63b293519147beb63ef83b6a
C1yde11B <Clydellb0@gmail.com>
2024-09-04 10:10:34 -0500

    add stuff

---
+++ WFC/View/SHV.swift
@@ -0,0 +1,40 @@
+//
+//  SHV.swift
+//  WFC
+//
+//  Created by AM Student on 9/4/24.
+//
+
+import SwiftUI
+
+struct SearchHeaderView: View {
+    
+    @State private var cityName = "Tulsa"
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .padding(.leading, 10 )
+            
+            ZStack {
+                // Creates an area for user input, binds the text the city cityname var
+                TextField("", text: $cityName)
+                    .padding(.leading, 5)
+                }
+            
+            
+            Image(systemName: "location.fill")
+                .padding(.trailing, 5)
+        }
+        
+        .foregroundColor(.white)
+        .padding()
+        .background(ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.blue.opacity(0.5))
+        })
+    }
+}
+#Preview {
+    SearchHeaderView()
+}
