a5606b58d5acb9498aa2bba109364179cbad273c
C1yde11B <Clydellb0@gmail.com>
2024-09-04 10:21:27 -0500

    CityView done

---
+++ WFC/View/CityNameView.swift
@@ -0,0 +1,26 @@
+//
+//  CityNameView.swift
+//  WFC
+//
+//  Created by AM Student on 9/4/24.
+//
+
+import SwiftUI
+
+struct CityNameView: View {
+    var city: String
+    var currentDate: String
+    
+    var body: some View {
+        VStack {
+            Text (city)
+                .font(.largeTitle)
+                .bold()
+            Text(currentDate)
+                .font(.title2)
+        }
+    }
+}
+#Preview {
+    CityNameView(city: "Tulsa", currentDate: "September 4, 2024")
+}
