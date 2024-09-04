c03c22562d208d0848adf4c5f33aa00635cce626
C1yde11B <Clydellb0@gmail.com>
2024-09-04 09:04:51 -0500

    Initial Commit

---
+++ WFC/WFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  WFCApp.swift
+//  WFC
+//
+//  Created by AM Student on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
