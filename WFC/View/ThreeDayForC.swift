f27e640e70ecb0514cd8a591085029136ee50311
C1yde11B <Clydellb0@gmail.com>
2024-09-09 08:36:52 -0500

    Three day cast is done

---
+++ WFC/View/ThreeDayForC.swift
@@ -0,0 +1,18 @@
+//
+//  ThreeDayForC.swift
+//  WFC
+//
+//  Created by AM Student on 9/5/24.
+//
+
+import SwiftUI
+
+struct ThreeDayForC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreeDayForC()
+}
